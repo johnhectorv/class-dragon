81d4205bf56bf3d606a092ea68fd5c291eb668be
TIFFANY <tiffanyliu2008@yahoo.com>
2023-05-18 20:37:18 -0700

    tiffany input

--- practice-for-week-04-class-dragons-long-practice-main/all-dragons.js
+++ practice-for-week-04-class-dragons-long-practice-main/all-dragons.js
@@ -1,5 +1,40 @@
-// Your code here
+const FriendlyDragon = require('./classes/friendly-dragon.js');
+const EvilDragon = require('./classes/evil-dragon.js');
+const Dragon = require('./classes/dragon.js');
 
+const falkor = new FriendlyDragon (
+  'Falkor',
+  'white',
+  ['save Atreyu from the swamp',
+  'save Atreyu from the Nothing',
+  'care the local bullies into a dumpster'
+  ],
+  'Bastia'
+  );
+
+const smaug = new EvilDragon (
+  'Smaug',
+  'black',
+  ["take over your mountain kingdom",
+  "steal all your dwarven gold",
+  "burn down your floating village"
+  ],
+  "Dwarf King"
+  );
+
+const allDragons = Dragon.getDragons(falkor, smaug);
+
+console.log(falkor);
+falkor.hasLifeGoals();
+console.log(falkor.breathesFire());
+console.log(falkor.helpsPeople());
+
+console.log(smaug);
+smaug.dontInviteThemOverForDinner();
+console.log(smaug.breathesFire());
+console.log(smaug.burnsNemesis());
+
+console.log(allDragons);
 
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
