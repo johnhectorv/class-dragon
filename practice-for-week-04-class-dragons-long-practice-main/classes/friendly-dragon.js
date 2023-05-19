5a287c2043cb4ab89bb7233755b37fecaf59030d
John valenzuela <johnhvalenzuela@gmail.com>
2023-05-18 23:11:51 -0400

    Johns Solution

--- practice-for-week-04-class-dragons-long-practice-main/classes/friendly-dragon.js
+++ practice-for-week-04-class-dragons-long-practice-main/classes/friendly-dragon.js
@@ -1,1 +1,36 @@
-// Your code here
+const Dragon = require('./dragon');
+
+class FriendlyDragon extends Dragon {
+    constructor(name, color, lifeGoals, friends){
+        super(name, color);
+        this.lifeGoals = lifeGoals;
+        this.friends = friends;
+    }
+
+    hasLifeGoals(){
+        this.lifeGoals.forEach(goals => {
+            console.log(`${this.name} likes to ${goals}`);
+        });
+    }
+
+    helpsPeople(){
+        return `${this.name} helps thier friend ${this.friend}`
+    }
+}
+
+const puff = new FriendlyDragon(
+    "Puff",
+    "green",
+    [
+      "live by the sea",
+      "frolick in the autumn mist",
+      "help small children"
+    ],
+    "Jackie Paper"
+  );
+  console.log(puff);
+  console.log(puff.breathesFire()); //Friendly dragons can still breathe fire!
+  puff.hasLifeGoals();
+  console.log(puff.helpsPeople());
+
+module.exports = FriendlyDragon;
